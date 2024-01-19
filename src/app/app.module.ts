ea5a2919f83e37f07e6369ff2ebcd67923856ff1
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2024-01-19 18:08:46 -0500

    formulario con validaciones ada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
